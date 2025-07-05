fb791f920bb5757c50e4badc0de50bebba559470
pardeep-pathania <pathanianikhil145@gmail.com>
2025-07-05 23:08:27 +0530

    dashboard and addcar page UI completed

--- client/src/components/owner/NavbarOwner.jsx
+++ client/src/components/owner/NavbarOwner.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { assets, dummyUserData } from '../../assets/assets'
+import { Link } from 'react-router-dom';
 
 const NavbarOwner = () => {
+
+const user = dummyUserData;
+
   return (
-    <div>
-      
+    <div className='flex items-center justify-between px-6 md:px-10 py-4 text-gray-500 border-b border-borderColor relative transition-all'>
+
+       <Link to='/'>
+         <h1 className='text-3xl text-primary font-bold'>Drive<span className='text-black'>Easy</span></h1>
+       </Link>
+      <p>Welcome, {user.name || "Owner"}</p>
+
     </div>
   )
 }
